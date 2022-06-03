6607712f26c35f2e1932ca099b7767ce2f3bac85
BlackDragon <73701060+BlackDragon0207@users.noreply.github.com>
2022-06-03 22:14:10 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 
 module.exports = async (client) => {
-    console.log('I am ready');            
-    client.user.setActivity(`BlackDragon Community`, { type: "COMPETING"})
+    console.log('I am ready');         
 }
